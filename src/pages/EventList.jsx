ff0c7bfa8b0e90065e13345b0c62c5a019bc136d
Anna Kazulak <anna.kazulak@gmail.com>
2023-09-28 19:26:19 +0200

    add search functionality to the event list

--- src/pages/EventList.jsx
+++ src/pages/EventList.jsx
@@ -5,13 +5,19 @@ import { Link } from "react-router-dom";
 function EventList() {
   const [events, setEvents] = useState([]);
 
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredEvents, setFilteredEvents] = useState([]);
+
   const getAllEvents = () => {
     const storedToken = localStorage.getItem("authToken");
     axios
       .get(`${import.meta.env.VITE_API_URL}/api/events`, {
         headers: { Authorization: `Bearer ${storedToken}` },
       })
-      .then((response) => setEvents(response.data))
+      .then((response) => {
+        setEvents(response.data);
+        setFilteredEvents(response.data);
+      })
       .catch((error) => console.log(error));
   };
 
@@ -19,11 +25,31 @@ function EventList() {
     getAllEvents();
   }, []);
 
+  // Function to handle search input changes
+  const handleSearchInput = (event) => {
+    const inputValue = event.target.value;
+    setSearchInput(inputValue);
+
+    // Filter the event list based on the search input
+    const filtered = events.filter((event) =>
+      event.title.toLowerCase().includes(inputValue.toLowerCase())
+    );
+    setFilteredEvents(filtered);
+  };
+
   return (
     <div className="container mt-5">
       <h1>All my Events</h1>
-
-      {events.map((event, index) => {
+      <div className="col-md-6">
+        <input
+          type="search"
+          placeholder="Search by event title"
+          className="form-control mb-3"
+          value={searchInput}
+          onChange={handleSearchInput}
+        />
+      </div>
+      {filteredEvents.map((event, index) => {
         return (
           <div key={event._id}>
             <ol className="list-group custom-list-group">
