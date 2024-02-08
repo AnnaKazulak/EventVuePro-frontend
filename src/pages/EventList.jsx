f390c53ca2946eae620b71eddf928f9fd80e9697
Anna Kazulak <anna.kazulak@gmail.com>
2024-02-08 22:16:29 +0100

    chane styles of event list

--- src/pages/EventList.jsx
+++ src/pages/EventList.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 
 function EventList() {
   const [events, setEvents] = useState([]);
-
   const [searchInput, setSearchInput] = useState("");
   const [filteredEvents, setFilteredEvents] = useState([]);
 
@@ -25,17 +24,35 @@ function EventList() {
     getAllEvents();
   }, []);
 
-  // Function to handle search input changes
-  const handleSearchInput = (event) => {
-    const inputValue = event.target.value;
-    setSearchInput(inputValue);
-
+  useEffect(() => {
     // Filter the event list based on the search input
     const filtered = events.filter((event) =>
-      event.title.toLowerCase().includes(inputValue.toLowerCase())
+      event.title.toLowerCase().includes(searchInput.toLowerCase())
     );
     setFilteredEvents(filtered);
+  }, [searchInput, events]);
+
+  const deleteEvent = async (eventId) => {
+    try {
+      const storedToken = localStorage.getItem("authToken");
+      await axios.delete(`${import.meta.env.VITE_API_URL}/api/events/${eventId}`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      });
+      // Refresh the events list after deletion
+      getAllEvents();
+    } catch (error) {
+      console.log(error);
+    }
   };
+  const currentDate = new Date();
+
+  const upcomingEvents = filteredEvents.filter(
+    (event) => new Date(event.date) > currentDate
+  );
+
+  const pastEvents = filteredEvents.filter(
+    (event) => new Date(event.date) <= currentDate
+  );
 
   return (
     <div className="container mt-5">
@@ -46,36 +63,81 @@ function EventList() {
           placeholder="Search by event title"
           className="form-control mb-3"
           value={searchInput}
-          onChange={handleSearchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
         />
       </div>
-      {filteredEvents.map((event, index) => {
-        return (
-          <div key={event._id}>
-            <ol className="list-group custom-list-group">
-              <Link
-                to={`/events/${event._id}`}
-                style={{ textDecoration: "none" }}
-              >
-                <li className="list-group-item d-flex justify-content-between align-items-start">
-                  <div className="ms-2 me-auto">
-                    <div className="fs-6 ">
-                      {" "}
-                      <span className="pe-4">{index + 1}.</span>
-                      {event.title}
-                    </div>
-                  </div>
-                  <div>
-                    <img src={event.imageUrl} alt="" className="custom-img" />
-                  </div>
-                </li>
-              </Link>
-            </ol>
-          </div>
-        );
-      })}
+
+      <h2>Upcoming Events</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col" style={{ width: "60%" }}>Event Name</th>
+            <th scope="col" style={{ width: "20%" }}>Date</th>
+            <th scope="col" style={{ width: "20%" }}>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {upcomingEvents.map((event, index) => (
+            <tr key={event._id}>
+              <td>
+                <Link to={`/events/${event._id}`} className="event-link">
+                  {event.title}
+                </Link>
+              </td>
+              <td>{event.date}</td>
+              <td>
+                <Link
+                  to={`/events/edit/${event._id}`}
+                  className="event-link me-4"
+                >
+                  <i className="fas fa-pencil-alt icon-link"></i>
+                </Link>
+                <span className="event-link" onClick={() => deleteEvent(event._id)}>
+                  <i className="fas fa-trash-alt icon-link"></i>
+                </span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h2>Past Events</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col" style={{ width: "60%" }}>Event Name</th>
+            <th scope="col" style={{ width: "20%" }}>Date</th>
+            <th scope="col" style={{ width: "20%" }}>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pastEvents.map((event, index) => (
+            <tr key={event._id}>
+              <td>
+                <Link to={`/events/${event._id}`} className="event-link">
+                  {event.title}
+                </Link>
+              </td>
+              <td>{event.date}</td>
+              <td>
+                <Link
+                  to={`/events/edit/${event._id}`}
+                  className="event-link me-4"
+                >
+                  <i className="fas fa-pencil-alt icon-link"></i>
+                </Link>
+                <span className="event-link" onClick={() => deleteEvent(event._id)}>
+                  <i className="fas fa-trash-alt icon-link"></i>
+                </span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
 
 export default EventList;
+
+
