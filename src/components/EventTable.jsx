f71b10e52654731fd7e68dda4619535868d17517
Anna Kazulak <anna.kazulak@gmail.com>
2024-02-15 16:53:20 +0100

    Integrate existing PageHeader component and extract EventTable from EventList, then integrate EventTable into EventList

---
+++ src/components/EventTable.jsx
@@ -0,0 +1,95 @@
+import  { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { formatDateShort } from '../utils/dateUtils';
+
+const EventTable = ({ title, events, deleteEvent }) => {
+  const [sortedEvents, setSortedEvents] = useState([]);
+  const [sortConfig, setSortConfig] = useState({ key: 'date', direction: 'asc' });
+
+  useEffect(() => {
+    // Sort events initially
+    sortEvents(sortConfig.key);
+  }, [events]);
+
+  // Function to handle sorting
+  const sortEvents = (key) => {
+    let direction = 'asc';
+    if (sortConfig.key === key && sortConfig.direction === 'asc') {
+      direction = 'desc';
+    }
+    setSortConfig({ key, direction });
+
+    const sorted = [...events].sort((a, b) => {
+      if (key === 'date') {
+        const dateA = new Date(a.date).getTime();
+        const dateB = new Date(b.date).getTime();
+        return direction === 'asc' ? dateA - dateB : dateB - dateA;
+      } else {
+        if (a[key] < b[key]) {
+          return direction === 'asc' ? -1 : 1;
+        }
+        if (a[key] > b[key]) {
+          return direction === 'asc' ? 1 : -1;
+        }
+        return 0;
+      }
+    });
+
+    setSortedEvents(sorted);
+  };
+
+  return (
+    <>
+      <h2>{title}</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col" style={{ width: "60%" }} className="pointer" onClick={() => sortEvents('title')}>
+              Event Name {sortConfig.key === 'title' && (
+                <i className={`fas fa-caret-${sortConfig.direction === 'asc' ? 'up' : 'down'}`}></i>
+              )}
+            </th>
+            <th scope="col" style={{ width: "20%" }} className="pointer" onClick={() => sortEvents('date')}>
+              Date {sortConfig.key === 'date' && (
+                <i className={`fas fa-caret-${sortConfig.direction === 'asc' ? 'up' : 'down'}`}></i>
+              )}
+            </th>
+            <th scope="col" style={{ width: "20%" }}>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedEvents.map((event) => (
+            <tr key={event._id}>
+              <td>
+                <Link to={`/events/${event._id}`} className="event-link">
+                  {event.title}
+                </Link>
+              </td>
+              <td>{formatDateShort(event.date)}</td>
+              <td>
+                <Link
+                  to={`/events/edit/${event._id}`}
+                  className="event-link me-4"
+                >
+                  <i className="fas fa-pencil-alt icon-link"></i>
+                </Link>
+                <span className="event-link" onClick={() => deleteEvent(event._id)}>
+                  <i className="fas fa-trash-alt icon-link"></i>
+                </span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+EventTable.propTypes = {
+  title: PropTypes.string.isRequired,
+  events: PropTypes.array.isRequired,
+  deleteEvent: PropTypes.func.isRequired
+};
+
+export default EventTable;
--- src/pages/EventList.jsx
+++ src/pages/EventList.jsx
@@ -1,151 +1,85 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import { formatDateShort } from '../utils/dateUtils';
+import PageHeader from "../components/PageHeader";
+import EventTable from "../components/EventTable";
 
 function EventList() {
-  const [events, setEvents] = useState([]);
-  const [searchInput, setSearchInput] = useState("");
-  const [filteredEvents, setFilteredEvents] = useState([]);
+    const [events, setEvents] = useState([]);
+    const [searchInput, setSearchInput] = useState("");
+    const [filteredEvents, setFilteredEvents] = useState([]);
 
-  const getAllEvents = () => {
-    const storedToken = localStorage.getItem("authToken");
-    axios
-      .get(`${import.meta.env.VITE_API_URL}/api/events`, {
-        headers: { Authorization: `Bearer ${storedToken}` },
-      })
-      .then((response) => {
-        const eventsData = response.data;
+    const getAllEvents = () => {
+        const storedToken = localStorage.getItem("authToken");
+        axios
+            .get(`${import.meta.env.VITE_API_URL}/api/events`, {
+                headers: { Authorization: `Bearer ${storedToken}` },
+            })
+            .then((response) => {
+                const eventsData = response.data;
 
-        // Format the date for each event
-        const formattedEvents = eventsData.map(event => {
-          const formattedEventDate = formatDateShort(event.date);
-          return { ...event, formattedDate: formattedEventDate };
-        });
+                // Format the date for each event
+                const formattedEvents = eventsData.map(event => {
+                    const formattedEventDate = formatDateShort(event.date);
+                    return { ...event, formattedDate: formattedEventDate };
+                });
 
-        // Update state with formatted events
-        setEvents(formattedEvents);
-        setFilteredEvents(formattedEvents);
-      })
-      .catch((error) => console.log(error));
-  };
+                // Update state with formatted events
+                setEvents(formattedEvents);
+                setFilteredEvents(formattedEvents);
+            })
+            .catch((error) => console.log(error));
+    };
 
-  useEffect(() => {
-    getAllEvents();
-  }, []);
+    useEffect(() => {
+        getAllEvents();
+    }, []);
 
-  useEffect(() => {
-    // Filter the event list based on the search input
-    const filtered = events.filter((event) =>
-      event.title.toLowerCase().includes(searchInput.toLowerCase())
+    useEffect(() => {
+        // Filter the event list based on the search input
+        const filtered = events.filter((event) =>
+            event.title.toLowerCase().includes(searchInput.toLowerCase())
+        );
+        setFilteredEvents(filtered);
+    }, [searchInput, events]);
+
+    const deleteEvent = async (eventId) => {
+        try {
+            const storedToken = localStorage.getItem("authToken");
+            await axios.delete(`${import.meta.env.VITE_API_URL}/api/events/${eventId}`, {
+                headers: { Authorization: `Bearer ${storedToken}` },
+            });
+            // Refresh the events list after deletion
+            getAllEvents();
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    const currentDate = new Date();
+
+    const upcomingEvents = filteredEvents.filter(
+        (event) => new Date(event.date) > currentDate
     );
-    setFilteredEvents(filtered);
-  }, [searchInput, events]);
 
-  const deleteEvent = async (eventId) => {
-    try {
-      const storedToken = localStorage.getItem("authToken");
-      await axios.delete(`${import.meta.env.VITE_API_URL}/api/events/${eventId}`, {
-        headers: { Authorization: `Bearer ${storedToken}` },
-      });
-      // Refresh the events list after deletion
-      getAllEvents();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const currentDate = new Date();
+    const pastEvents = filteredEvents.filter(
+        (event) => new Date(event.date) <= currentDate
+    );
 
-  const upcomingEvents = filteredEvents.filter(
-    (event) => new Date(event.date) > currentDate
-  );
+    return (
+        <div className="container mt-5">
 
-  const pastEvents = filteredEvents.filter(
-    (event) => new Date(event.date) <= currentDate
-  );
+            <PageHeader
+                title="All my Events"
+                itemCount={filteredEvents.length}
+                searchInput={searchInput}
+                handleSearchInput={(e) => setSearchInput(e.target.value)}
+            />
 
-  return (
-    <div className="container mt-5">
-      <h1>All my Events</h1>
-      <div className="col-md-6">
-        <input
-          type="search"
-          placeholder="Search by event title"
-          className="form-control mb-3"
-          value={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)}
-        />
-      </div>
+            <EventTable title="Upcoming Events" events={upcomingEvents} deleteEvent={deleteEvent} />
+            <EventTable title="Past Events" events={pastEvents} deleteEvent={deleteEvent} />
 
-      <h2>Upcoming Events</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col" style={{ width: "60%" }}>Event Name</th>
-            <th scope="col" style={{ width: "20%" }}>Date</th>
-            <th scope="col" style={{ width: "20%" }}>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {upcomingEvents.map((event) => (
-            <tr key={event._id}>
-              <td>
-                <Link to={`/events/${event._id}`} className="event-link">
-                  {event.title}
-                </Link>
-              </td>
-              <td>{formatDateShort(event.date)}</td>
-              <td>
-                <Link
-                  to={`/events/edit/${event._id}`}
-                  className="event-link me-4"
-                >
-                  <i className="fas fa-pencil-alt icon-link"></i>
-                </Link>
-                <span className="event-link" onClick={() => deleteEvent(event._id)}>
-                  <i className="fas fa-trash-alt icon-link"></i>
-                </span>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-      <h2>Past Events</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col" style={{ width: "60%" }}>Event Name</th>
-            <th scope="col" style={{ width: "20%" }}>Date</th>
-            <th scope="col" style={{ width: "20%" }}>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {pastEvents.map((event) => (
-            <tr key={event._id}>
-              <td>
-                <Link to={`/events/${event._id}`} className="event-link">
-                  {event.title}
-                </Link>
-              </td>
-              <td>{formatDateShort(event.date)}</td>
-              <td>
-                <Link
-                  to={`/events/edit/${event._id}`}
-                  className="event-link me-4"
-                >
-                  <i className="fas fa-pencil-alt icon-link"></i>
-                </Link>
-                <span className="event-link" onClick={() => deleteEvent(event._id)}>
-                  <i className="fas fa-trash-alt icon-link"></i>
-                </span>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default EventList;
