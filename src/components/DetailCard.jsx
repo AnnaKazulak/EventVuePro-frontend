5feb4e41b6810f63e4fb40a4a51313fdce7d526b
Anna Kazulak <anna.kazulak@gmail.com>
2024-03-01 18:50:36 +0100

    remove inline styled, bug resolve: add styled to make the card look good for large screens

--- src/components/DetailCard.jsx
+++ src/components/DetailCard.jsx
@@ -4,13 +4,12 @@ import { Link } from "react-router-dom";
 const DetailCard = ({ data, basePath }) => {
     return (
         <div className="col">
-            <Link to={`${basePath}/${data._id}`} style={{ textDecoration: "none" }}>
+            <Link to={`${basePath}/${data._id}`} className="guest-card-link">
                 <div className="card h-100 shadow guest-card">
                     <img
                         src={data.imageUrl}
                         className="card-img-top"
                         alt={data.name}
-                        style={{ objectFit: "cover", height: "200px" }}
                     />
                     <div className="card-body">
                         <h5 className="card-title">{data.name}</h5>
