4fd9e8b050170af3a7895b10a289350496d236cb
Anna Kazulak <anna.kazulak@gmail.com>
2024-03-04 18:01:24 +0100

    add smooth scrolling

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -5,7 +5,10 @@ function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // Smooth scrolling
+    });
   }, [pathname]);
 
   return null;
