b88031c41c7aab1dbaab12a3ced20c1fc4b25423
Anna Kazulak <anna.kazulak@gmail.com>
2024-03-06 11:19:23 +0100

    add small design changes

--- src/components/EntityDetails .jsx
+++ src/components/EntityDetails .jsx
@@ -13,38 +13,36 @@ const EntityDetails = ({
 }) => {
 
     return (
-        <div className="container-wrapper">
-            <div className="container" >
-                <div className="row">
-                    <div className="col-md-6">
-                        <img
-                            src={imageUrl}
-                            alt={name}
-                            className="img-fluid shadow-lg mb-5 bg-body rounded custom-img-details"
-                        />
-                    </div>
-                    <div className="col-md-6">
-                        <div className="row mb-3">
-                            <div className="col-md-12">
-                                <h3 className=" mb-4">{title}</h3>
-                            </div>
-                            <div className="col-md-12">
-                                <h3 className=" mb-4">{name}</h3>
-                            </div>
-                            <div className="col-md-12">
-                                <p><strong>{descriptionLabel}</strong> {description}</p>
-                            </div>
-                            <div className="col-md-12">
-                                <p><strong>{dateLabel}</strong> {date}</p>
-                            </div>
-                            <div className="col-md-12">
-                                <p><strong>{locationLabel}</strong> {location}</p>
-                            </div>
+        <div className="container-wrapper mx-5">
+            <div className="row">
+                <div className="col-md-6">
+                    <img
+                        src={imageUrl}
+                        alt={name}
+                        className="img-fluid shadow-lg mb-5 bg-body rounded custom-img-details"
+                    />
+                </div>
+                <div className="col-md-6">
+                    <div className="row mb-3">
+                        <div className="col-md-12">
+                            <h3 className=" mb-4">{title}</h3>
                         </div>
-                        <div className="row mb-3">
-                            <div className="col-md-12">
-                                <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-                                </div>
+                        <div className="col-md-12">
+                            <h3 className=" mb-4">{name}</h3>
+                        </div>
+                        <div className="col-md-12">
+                            <p><strong>{descriptionLabel}</strong> {description}</p>
+                        </div>
+                        <div className="col-md-12">
+                            <p><strong>{dateLabel}</strong> {date}</p>
+                        </div>
+                        <div className="col-md-12">
+                            <p><strong>{locationLabel}</strong> {location}</p>
+                        </div>
+                    </div>
+                    <div className="row mb-3">
+                        <div className="col-md-12">
+                            <div className="d-grid gap-2 d-md-flex justify-content-md-start">
                             </div>
                         </div>
                     </div>
